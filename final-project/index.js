494c6e1a863baf92d0a38b69e403daa3ecd43ba1
Ellie Pearson <epearso4@umd.edu>
2020-05-02 16:34:17 -0700

    update

--- final-project/index.js
+++ final-project/index.js
@@ -302,8 +302,10 @@ io.on('connection', function(socket) {
 });
 
 
-http.listen(3000, function() {
-    console.log('Listening on port 3000!');
-});
+// http.listen(3000, function() {
+//     console.log('Listening on port 3000!');
+// });
 
-// http.listen(process.env.PORT || 3000), function() {console.log("listening")}
+http.listen(process.env.PORT || 3000, function() {
+    console.log('Listening!');
+})
